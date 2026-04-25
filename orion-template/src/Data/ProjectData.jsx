3ee08f1ae4dc33e91ec11edf261e208ac08f0fcf
0r10n650 <orion4cooler@gmail.com>
2026-04-24 22:34:14 -0600

    Trying to get stuff back

--- orion-template/src/Data/ProjectData.jsx
+++ orion-template/src/Data/ProjectData.jsx
@@ -21,23 +21,14 @@ export const PROJECTS = {
     startDate: "March 2026",
     endDate: "",
     currentState: "Active",
-     notes: `This project began as something small that I could finish and polish to put on a portfolio, 
-     but like everything I make, it never stays small. 
-     I did get it to a state I am happy to put it on my portfolio 
-     but I also have a lot of ideas about where to take the game from this point. 
-     My current focus is making the movement look better and making the game more 
-     professional in general. The plan after that is to add upgrades and new enemies to add more variety.`,
+     notes: "This project began as something small that I could finish and polish to put on a portfolio, but like everything I make, it never stays small. I did get it to a state I am happy to put it on my portfolio but I also have a lot of ideas about where to take the game from this point. My current focus is making the movement look better and making the game more professional in general. The plan after that is to add upgrades and new enemies to add more variety.",
     gallery: "asteroidsPlus",
   },
   gmky: {
     title: "D&D Inspired Card Game",
     realTitle: "The GM is Trying To Kill You",
     shortDescription: "One vs All card game transforming TTRPG combat into a strategic deck builder.",
-    description: `A One vs All card game hevily based off of modern fantary genres. 
-    Your friend, the Game Master, has turned against you and your party. Your constant 
-    bad manners and terrible role playing has finally worn him down. Now he seeks revenge. 
-    Will you and your friends defeat his challenges, or will he kill you all and show you 
-    once and for all who runs this game?`,
+    description: "A One vs All card game hevily based off of modern fantary genres. Your friend, the Game Master, has turned against you and your party. Your constant bad manners and terrible role playing has finally worn him down. Now he seeks revenge. Will you and your friends defeat his challenges, or will he kill you all and show you once and for all who runs this game?",
     highlights: [
       "Playtest and developed as part of a small team",
       "Learned Blender to 3d model game tiles",
@@ -56,18 +47,7 @@ export const PROJECTS = {
     startDate: "Jan 2025",
     endDate: "",
     currentState: "Active",
-    notes: `This project has been in the making for a long time. 
-    My original plan was to make my own TTRPG called "Sea of Steam." 
-    I still plan to make that but there were several ambitous pieces that I needed to work out. 
-    One of them was the idea of a card game for combat. Long story short, its a lot of work to make that happen. 
-    So me and a couple of my friends decided to create this project. 
-    Instead of trying to develop the card game with everything else, we would isolate the card game
-    and turn it into its own game. It still needs a lot of work but we think its got a lot of good
-    ideas and gameplay elements. This project has taught me the most about the design process and 
-    how difficult it is to keep thing moving and developing. We spent a lot of time developing the 
-    game in directions it really didn't need to go. We learned from those mistakes and then made 
-    more progress in a couple weeks than the previous months combined. Right now we are still 
-    making rough prototypes and changing the rules fairly drastically. More playtests are required.`,
+    notes: "",
     gallery: "GMKY",
   },
   cardeditor: {
@@ -84,19 +64,13 @@ export const PROJECTS = {
     ],
     skillsLearned: [
       "Object oriented coding",
-      "Data management",
-      "Save and load systems",
-      "How to connect data elements together",
-      "Pick the correct tool for the job"
+      ""
     ],
     softwareUsed: "Visual Studio, Godot, C#, GDScript",
     startDate: "May 2024",
     endDate: "Feb 2025",
     currentState: "Completed",
-    notes: `I made this app to help make card for another project "Sea of Steam" which is now "The GM is Trying to Kill You." 
-    I started off coding in C# using .net maui. A while later I realized that since this was being made for a game, wouldn't
-    a game engine make more sense for development? Thus I remade it using Godot and that went a lot better. The hardest part
-    was managing the data and I definitely could've made it better but I finished the thing and it worked. Good enough.`,
+    notes: "",
     gallery: "cardEditor",
   },
   volleyballAnimation: {
@@ -111,20 +85,13 @@ export const PROJECTS = {
       "Learned about what details should be focused on"
     ],
     skillsLearned: [
-      "Animation takes a long time, especially when hand drawn",
-      "Focus on the whole picture first",
-      "A small amount of detail over every frame is better than one frame thats perfect",
-      "Improved drawing abilities and recognizing shadows"
+
     ],
     softwareUsed: "Davinci Resolve",
     startDate: "April 2025",
     endDate: "May 2025",
     currentState: "Completed",
-    notes: `This project was a lot of fun. I did it for the final of my drawing class at USU. It felt ambitous to try
-    and do a flip book animation, but I was fascinated with impact frame at the time. I still am I suppose. While 
-    adding details to one of the frames, I realized that I simply didn't have enough time to put that much detail 
-    on all of the frames. I only realized when I was in pressure mode that I needed to focus on the frames that
-    were the most memorable. Not every frame needs that much detail. I try and keep that with me.`,
+    notes: "",
     gallery: "vbAnimation"
   },
   ITLS: {
@@ -139,25 +106,20 @@ export const PROJECTS = {
       "Learned how to use the sculpting tool",
     ],
     skillsLearned: [
-      "How to add and manipulate objects in Blender",
-      "The sculpt tool in Blender",
-      "How to rig a skeleton in Blender",
-      "Blenders animation system"
+
     ],
     softwareUsed: "Blender, Unity, Krita",
     startDate: "Jan 2025",
     endDate: "May 2025",
     currentState: "Completed",
-    notes: `These are all of the projects from one of my classes. I think I definetly could've tried a little harder,
-    but the class wasn't super great so I focused my efforts elsewhere. I learned the basics of blender though, and
-    that was nice.`,
+    notes: "",
     gallery: "ITLS"
   },
   nuclearEnergy: {
     title: "Nuclear Energy Informational Animatic",
     realTitle: "Nuclear Energy Video",
     shortDescription: "Animatic made in Blender aiming to inform about the benefits of nuclear energy.",
-    description: "Short animation informing about the positives of nuclear energy. https://youtu.be/bHhlTORibZM",
+    description: "https://youtu.be/bHhlTORibZM",
     highlights: [
       "Learned how to use Blender for 2d animation",
       "Found a way to have an informational video also be entertaining",
@@ -165,18 +127,13 @@ export const PROJECTS = {
       "Wrote and recorded voice over for the project",
     ],
     skillsLearned: [
-      "How to use Blenders 2D animation tools",
-      "How to record a voice over",
-      "How to edit a video using Adobe Premiere Pro",
-      "How to adapt research to mediums other than essays"
+      
     ],
     softwareUsed: "Blender, Adobe Premiere Pro",
     startDate: "April 2022",
     endDate: "May 2022",
     currentState: "Completed",
-    notes: `I made this for my senior project and I definetly underestimated it. Animation takes a lot more time and 
-    work than anticipated. I was still happy with how it turned out and my teacher also really liked it. I learned 
-    a lot about making animation and got to figure out some aspects of blender so I was happy.`,
+    notes: "",
     gallery: ""
   },
   sisyphus: {
@@ -191,19 +148,13 @@ export const PROJECTS = {
       "Realized my limitations and expectations for the project, leading to a ongoing hiatus to complete smaller projects"
     ],
     skillsLearned: [
-      "State machines and their place in game development",
-      "Godots physics system",
-      "Importing assets from Blender to Godot",
-      "Focus on the big picture",
-      "Have clear design requirements and goals",
+
     ],
     softwareUsed: "Godot, Blender, GDScript",
     startDate: "Feb 2025",
     endDate: "",
     currentState: "Hiatus",
-    notes: `Sisyphus is a game that I want to be really good. I started making it, realized that I don't have any 
-    experience in game design, and have promptly set it aside for once I get some small projects under my belt. And
-    thats where it remains, festering in my subconsious until I feel ready to slay the beast it truly is. `,
+    notes: "",
     gallery: "sisyphus"
   },
   portfolio: {
@@ -218,19 +169,13 @@ export const PROJECTS = {
       "Developed a system to easily be able to add more to my portfolio as needed"
     ],
     skillsLearned: [
-      "React and how it affect website development",
-      "Tailwind CSS and how it makes css so much easier",
-      "How to pull data from files",
-      "How to code in a more scaleable way"
+
     ],
     softwareUsed: "Visual Studio Code, GitHub, React, TaiwindCSS, JavaScript XML",
     startDate: "April 2026",
     endDate: "",
     currentState: "Active",
-    notes: `Making my own website portfolio has been very rewarding. It feels nice to know that all of my work is also 
-    stored on another part of my work and not just some generic website. It has also been a great learning opportunity to
-    teach myself React and Tailwind. I also found out just how awesome GitHub is while making this. Its so much easier
-    than I previously thought. I'm going to try and add as many of my projects to GitHub as I can.`,
+    notes: "",
     gallery: ""
   }
 }
--- orion-template/src/Helper/Sidebar.jsx
+++ orion-template/src/Helper/Sidebar.jsx
@@ -5,23 +5,18 @@ import { getSortedProjects } from "./Sorter"
 export default function Sidebar() {
   return (
     <div className="pt-3 pr-3 items-center bg-stone-900 space-y-4 h-lvh min-w-80">
-      <div class="mx-auto max-w-sm items-center p-6 rounded-r-xl bg-stone-800 shadow-lg outline outline-black/5 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
       <Link to="/" className=" hover:text-gray-400">
-        <h1 className="text-4xl font-bold mb-4 text-black dark:text-white">
-          Orion Rand
-        </h1>
+        <div class="mx-auto max-w-sm items-center p-6 rounded-r-xl bg-stone-800 shadow-lg outline outline-black/5 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+              <h1 className="text-4xl font-bold mb-4 text-black dark:text-white">
+            Orion Rand
+            </h1>
+            <p className="mb-6 text-gray-400">
+                Portfolio of everything I've done.
+                <br/>
+                orion.w.rand@gmail.com
+            </p>
+        </div>
       </Link>
-        <p className="mb-6 text-gray-400">
-            Portfolio of everything I've done.
-            <br/>
-            orion.w.rand@gmail.com
-            <br/>
-            <Link to="https://github.com/0r10n650">
-              https://github.com/0r10n650
-            </Link>
-            
-        </p>
-    </div>
         <div className="p-3 bg-zinc-700 text-white rounded-r-lg space-y-2">
         {getSortedProjects(PROJECTS).map(([key, project]) => (
           <Link
